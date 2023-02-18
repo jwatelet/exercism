fe4c6dcd9acaf6774f7eba23d887722cd6e0fca9
Johan Watelet <johanwatelet@gmail.com>
2023-02-18 17:34:49 +0100

    locomotive engineer

---
+++ locomotive-engineer/locomotive_engineer.rb
@@ -0,0 +1,20 @@
+class LocomotiveEngineer
+  def self.generate_list_of_wagons(*wagons)
+    wagons
+  end
+
+  def self.fix_list_of_wagons(each_wagons_id, missing_wagons)
+    first, second, third, *rest = each_wagons_id
+
+    [third, *missing_wagons, *rest, first, second]
+  end
+
+  def self.add_missing_stops(route, **stops)
+    route[:stops] = stops.values
+    route
+  end
+
+  def self.extend_route_information(route, more_route_information)
+    route.merge(more_route_information)
+  end
+end
